eec6784b0aa9775f31e3de8fab507a3b16ecedd2
pinchwang <mac@pinch.local>
2017-06-13 17:02:37 +0800

    新建工程

---
+++ MahjongAccounting/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MahjongAccounting
+//
+//  Created by mac on 2017/6/13.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var button : UIButton?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MahjongAccountingTests/MahjongAccountingTests.swift
@@ -0,0 +1,37 @@
+//
+//  MahjongAccountingTests.swift
+//  MahjongAccountingTests
+//
+//  Created by mac on 2017/6/13.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+import XCTest
+@testable import MahjongAccounting
+
+class MahjongAccountingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("自定义测试")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
